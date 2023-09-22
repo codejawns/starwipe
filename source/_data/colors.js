763e5c3a8bf09916a4db13cbe32bfe0d5b24c546
Reed Piernock <reed.piernock@georgetown.edu>
2023-09-21 21:35:38 -0400

    add a11y colors

---
+++ source/_data/colors.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    name: "black",
+    color: "#333232"
+  },
+  {
+    name: "gray",
+    color: "#6b6868ff"
+  },
+  {
+    name: "blue",
+    color: "#79d4d9ff"
+  },
+  {
+    name: "yellow",
+    color: "#f7ce5bff"
+  },
+  {
+    name: "orange",
+    color: "#e9724cff"
+  },
+  {
+    name: "white",
+    color: "#fdf1edff"
+  }
+]
