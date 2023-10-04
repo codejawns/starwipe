faa077d71a563767a6d12ad55c633b4c35c3aad2
Reed Piernock <reed.piernock@georgetown.edu>
2023-10-04 00:51:04 -0400

    add color vars, utils for base, tones

--- source/_data/colors.js
+++ source/_data/colors.js
@@ -1,26 +1,114 @@
 module.exports = [
   {
-    name: "black",
-    color: "#333232ff"
+    name: "primary-light",
+    color: "#b20089"
   },
   {
-    name: "gray",
-    color: "#6b6868ff"
+    name: "primary-alt-light",
+    color: "#ffd8eb"
   },
   {
-    name: "blue",
-    color: "#79d4d9ff"
+    name: "secondary-light",
+    color: "#00696b"
   },
   {
-    name: "yellow",
-    color: "#f7ce5bff"
+    name: "secondary-alt-light",
+    color: "#62f7fa"
   },
   {
-    name: "orange",
-    color: "#e9724cff"
+    name: "tertiary-light",
+    color: "#6a5f00"
   },
   {
-    name: "white",
-    color: "#fdf1edff"
+    name: "tertiary-alt-light",
+    color: "#f8e451"
+  },
+  {
+    name: "alert-light",
+    color: "#ba1a1a"
+  },
+  {
+    name: "alert-alt-light",
+    color: "#ffdad6"
+  },
+  {
+    name: "background-light",
+    color: "#f7ffec"
+  },
+  {
+    name: "background-alt-light",
+    color: "#f7ffec"
+  },
+  {
+    name: "foreground-light",
+    color: "#082100"
+  },
+  {
+    name: "foreground-alt-light",
+    color: "#082100"
+  },
+  {
+    name: "outline-light",
+    color: "#817379"
+  },
+  {
+    name: "outline-alt-light",
+    color: "#4f4349"
+  },
+  {
+    name: "primary-dark",
+    color: "#ffaedc"
+  },
+  {
+    name: "primary-alt-dark",
+    color: "#880068"
+  },
+  {
+    name: "secondary-dark",
+    color: "#3bdbde"
+  },
+  {
+    name: "secondary-alt-dark",
+    color: "#004f51"
+  },
+  {
+    name: "tertiary-dark",
+    color: "#dbc836"
+  },
+  {
+    name: "tertiary-alt-dark",
+    color: "#504700"
+  },
+  {
+    name: "alert-dark",
+    color: "#ffb4ab"
+  },
+  {
+    name: "alert-alt-dark",
+    color: "#93000a"
+  },
+  {
+    name: "background-dark",
+    color: "#082100"
+  },
+  {
+    name: "background-alt-dark",
+    color: "#082100"
+  },
+  {
+    name: "foreground-dark",
+    color: "#f7ffec"
+  },
+  {
+    name: "foreground-alt-dark",
+    color: "#f7ffec"
+  },
+  {
+    name: "outline-dark",
+    color: "#9c8d93"
+  },
+  {
+    name: "outline-alt-dark",
+    color: "#d3c2c9"
   }
 ]
