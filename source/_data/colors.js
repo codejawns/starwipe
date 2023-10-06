5c06b4c6af53476b24a84cc9d227887c44088e68
Reed Piernock <reed.piernock@georgetown.edu>
2023-10-06 18:22:41 -0400

    update colors

--- source/_data/colors.js
+++ source/_data/colors.js
@@ -1,114 +1,22 @@
 module.exports = [
   {
-    name: "primary-light",
-    color: "#b20089"
+    name: "primary",
+    color: "#227c9d"
   },
   {
-    name: "primary-alt-light",
-    color: "#ffd8eb"
+    name: "secondary",
+    color: "#17c3b2"
   },
   {
-    name: "secondary-light",
-    color: "#00696b"
+    name: "tertiary",
+    color: "#ffcb77"
   },
   {
-    name: "secondary-alt-light",
-    color: "#62f7fa"
+    name: "error",
+    color: "#fe6d73"
   },
   {
-    name: "tertiary-light",
-    color: "#6a5f00"
-  },
-  {
-    name: "tertiary-alt-light",
-    color: "#f8e451"
-  },
-  {
-    name: "alert-light",
-    color: "#ba1a1a"
-  },
-  {
-    name: "alert-alt-light",
-    color: "#ffdad6"
-  },
-  {
-    name: "background-light",
-    color: "#f7ffec"
-  },
-  {
-    name: "background-alt-light",
-    color: "#f7ffec"
-  },
-  {
-    name: "foreground-light",
-    color: "#082100"
-  },
-  {
-    name: "foreground-alt-light",
-    color: "#082100"
-  },
-  {
-    name: "outline-light",
-    color: "#817379"
-  },
-  {
-    name: "outline-alt-light",
-    color: "#4f4349"
-  },
-  {
-    name: "primary-dark",
-    color: "#ffaedc"
-  },
-  {
-    name: "primary-alt-dark",
-    color: "#880068"
-  },
-  {
-    name: "secondary-dark",
-    color: "#3bdbde"
-  },
-  {
-    name: "secondary-alt-dark",
-    color: "#004f51"
-  },
-  {
-    name: "tertiary-dark",
-    color: "#dbc836"
-  },
-  {
-    name: "tertiary-alt-dark",
-    color: "#504700"
-  },
-  {
-    name: "alert-dark",
-    color: "#ffb4ab"
-  },
-  {
-    name: "alert-alt-dark",
-    color: "#93000a"
-  },
-  {
-    name: "background-dark",
-    color: "#082100"
-  },
-  {
-    name: "background-alt-dark",
-    color: "#082100"
-  },
-  {
-    name: "foreground-dark",
-    color: "#f7ffec"
-  },
-  {
-    name: "foreground-alt-dark",
-    color: "#f7ffec"
-  },
-  {
-    name: "outline-dark",
-    color: "#9c8d93"
-  },
-  {
-    name: "outline-alt-dark",
-    color: "#d3c2c9"
+    name: "neutral",
+    color: "#3c3c3b"
   }
 ]
