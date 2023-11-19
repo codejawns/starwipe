77a2ea692464735225d2a2f2d1b7e51591395206
Reed Piernock <18013388+reedcodes@users.noreply.github.com>
2023-11-19 14:49:25 -0500

    Change output date formats (#8)
    * update date formats and presentation date structure
    
    * update gitignore
    
    ---------
    
    Co-authored-by: Reed Piernock <reed.piernock@georgetown.edu>

--- source/_config/filters/date-short.js
+++ source/_config/filters/date-short.js
@@ -1,9 +1,9 @@
 // Import date methods.
 const {DateTime} = require("luxon");
 
-// Returns a date in the format of day-month-year. e.g. "1 January 2023".
+// Returns a date in the format of day-month-year. e.g. "2023-01-01".
 module.exports = function(dateObj) {
   return DateTime
     .fromISO(dateObj)
-    .toFormat("d MMM yyyy");
+    .toFormat("yyyy-MM-dd");
 };
--- source/presentation/presentation.11tydata.js
+++ source/presentation/presentation.11tydata.js
@@ -1,6 +1,6 @@
 module.exports = {
   layout: "presentation",
-  permalink: "/presentation/{{ page.fileSlug }}/",
+  permalink: "/presentation/{{- date | dateShort -}}-{{- page.fileSlug -}}/",
   tags: "presentation",
   eleventyComputed: {
     eleventyNavigation: {
