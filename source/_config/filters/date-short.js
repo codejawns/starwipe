041343c0176af32badc9214e7df47f687f7f4c89
Reed Piernock <agile.plan2042@snooble.com>
2024-09-13 18:49:17 -0400

    remove more start-over files

--- source/_config/filters/date-short.js
+++
@@ -1,9 +0,0 @@
-// Import date methods.
-const {DateTime} = require("luxon");
-
-// Returns a date in the format of day-month-year. e.g. "2023-01-01".
-module.exports = function(dateObj) {
-  return DateTime
-    .fromISO(dateObj)
-    .toFormat("yyyy-MM-dd");
-};
