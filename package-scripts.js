c4a896c38b05748fb385c8ad263cc61febd0938b
Reed Piernock <agile.plan2042@snooble.com>
2024-09-13 19:30:57 -0400

    update packages

---
+++ package-scripts.js
@@ -0,0 +1,42 @@
+/**
+ * npm scripts in the `package.json` file are all well and good, but sometimes
+ * they can get cumbersome. Using a resource like nps (npm package scripts) can
+ * help to keep things neat and tidy, making script more readable.
+ */
+module.exports = {
+  scripts: {
+    // Scripts for building the accessible color palette.
+    color: {
+      // There can be multiple scripts here, one for each color palette there
+      // may be! Some presentations (or all of them) may use one palette while
+      // others use a different one. The one listed here is for the main, base
+      // palette, and others here for example -- or to use!
+      base: 'mkdir -p ./source/_scss/colors/base && npx a11y-color-tokens --colorTokensPath=./source/_scss/colors/base/tokens.js --outputDirPath=./source/_scss/colors/base --outputFilename=_tokens.scss --sassOutputName=base --no-tokenPrefix --no-includeCustomProperties --compatibilityDocsPath=./source/_scss/colors/base --compatibilityDocsTitle="Base Palette Color Contrast"',
+
+      // Add a light color palette.
+      light: 'mkdir -p ./source/_scss/colors/light && npx a11y-color-tokens --colorTokensPath=./source/_scss/colors/light/tokens.js --outputDirPath=./source/_scss/colors/light --outputFilename=_tokens.scss --sassOutputName=light --no-tokenPrefix --no-includeCustomProperties --compatibilityDocsPath=./source/_scss/colors/light --compatibilityDocsTitle="Light Palette Color Contrast"',
+
+      // Add a dark color palette.
+      dark: 'mkdir -p ./source/_scss/colors/dark && npx a11y-color-tokens --colorTokensPath=./source/_scss/colors/dark/tokens.js --outputDirPath=./source/_scss/colors/dark --outputFilename=_tokens.scss --sassOutputName=dark --no-tokenPrefix --no-includeCustomProperties --compatibilityDocsPath=./source/_scss/colors/dark --compatibilityDocsTitle="Dark Palette Color Contrast"',
+
+      // Build all the accessible color palettes.
+      build: 'nps color.base color.light color.dark',
+
+      // The watch script will watch _any_ list of color tokens, and build the
+      // generated list(s).
+      watch: 'onchange "./source/_scss/colors/**/tokens.js -- nps color.build"'
+    },
+
+     // Scripts for building CSS files.
+    css: {
+      build: 'sass --load-path=./source/_scss --style=compressed --no-source-map ./source/_scss:./site/_assets/css',
+      watch: 'sass --watch --load-path=./source/_scss ./source/_scss:./site/_assets/css'
+    },
+
+    // Scripts for the static site.
+    site: {
+      build: 'npx @11ty/eleventy --quiet',
+      watch: 'npx @11ty/eleventy --serve --quiet'
+    }
+  }
+}
