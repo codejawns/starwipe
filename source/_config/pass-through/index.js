7233ee1964d37477f7ace3db136f45d8c2f0fdcb
Reed Piernock <agile.plan2042@snooble.com>
2024-09-14 19:38:08 -0400

    add custom fonts

--- source/_config/pass-through/index.js
+++ source/_config/pass-through/index.js
@@ -1,8 +1,17 @@
 module.exports = eleventyConfig => {
 
   eleventyConfig.addPassthroughCopy({
+    // Pass through reveal JavaScript functionality files.
     './node_modules/reveal.js/dist/reveal.js*': '_assets/js/',
     './node_modules/reveal.js/plugin/': '_assets/js/plugin/',
+
+    // Pass through fonts.
+    './node_modules/@fontsource-variable/fraunces/README.md': '_assets/css/fonts/fraunces/README.md',
+    './node_modules/@fontsource-variable/fraunces/index.css': '_assets/css/fonts/fraunces/index.css',
+    './node_modules/@fontsource-variable/fraunces/files/': '_assets/css/fonts/fraunces/files/',
+    './node_modules/@fontsource-variable/public-sans/README.md': '_assets/css/fonts/public-sans/README.md',
+    './node_modules/@fontsource-variable/public-sans/index.css': '_assets/css/fonts/public-sans/index.css',
+    './node_modules/@fontsource-variable/public-sans/files/': '_assets/css/fonts/public-sans/files/',
   });
 
 }
