e6c3f9cf1bd1066fb42de581630e1143125947a5
Reed Piernock <agile.plan2042@snooble.com>
2024-09-16 16:23:26 -0400

    update color scripts

--- package-scripts.js
+++ package-scripts.js
@@ -11,20 +11,20 @@ module.exports = {
       // may be! Some presentations (or all of them) may use one palette while
       // others use a different one. The one listed here is for the main, base
       // palette, and others here for example -- or to use!
-      base: 'mkdir -p ./source/_scss/colors/base && npx a11y-color-tokens --colorTokensPath=./source/_scss/colors/base/tokens.js --outputDirPath=./source/_scss/colors/base --outputFilename=_tokens.scss --sassOutputName=base --no-tokenPrefix --no-includeCustomProperties --compatibilityDocsPath=./source/_scss/colors/base --compatibilityDocsTitle="Base Palette Color Contrast"',
+      base: 'mkdir -p ./source/_scss/global/colors/base && npx a11y-color-tokens --colorTokensPath=./source/_scss/global/colors/base/tokens.js --outputDirPath=./source/_scss/global/colors/base --outputFilename=_tokens.scss --sassOutputName=base --no-tokenPrefix --no-includeCustomProperties --compatibilityDocsPath=./source/_scss/global/colors/base --compatibilityDocsTitle="Base Palette Color Contrast"',
 
       // Add a light color palette.
-      light: 'mkdir -p ./source/_scss/colors/light && npx a11y-color-tokens --colorTokensPath=./source/_scss/colors/light/tokens.js --outputDirPath=./source/_scss/colors/light --outputFilename=_tokens.scss --sassOutputName=light --no-tokenPrefix --no-includeCustomProperties --compatibilityDocsPath=./source/_scss/colors/light --compatibilityDocsTitle="Light Palette Color Contrast"',
+      light: 'mkdir -p ./source/_scss/global/colors/light && npx a11y-color-tokens --colorTokensPath=./source/_scss/global/colors/light/tokens.js --outputDirPath=./source/_scss/global/colors/light --outputFilename=_tokens.scss --sassOutputName=light --no-tokenPrefix --no-includeCustomProperties --compatibilityDocsPath=./source/_scss/global/colors/light --compatibilityDocsTitle="Light Palette Color Contrast"',
 
       // Add a dark color palette.
-      dark: 'mkdir -p ./source/_scss/colors/dark && npx a11y-color-tokens --colorTokensPath=./source/_scss/colors/dark/tokens.js --outputDirPath=./source/_scss/colors/dark --outputFilename=_tokens.scss --sassOutputName=dark --no-tokenPrefix --no-includeCustomProperties --compatibilityDocsPath=./source/_scss/colors/dark --compatibilityDocsTitle="Dark Palette Color Contrast"',
+      dark: 'mkdir -p ./source/_scss/global/colors/dark && npx a11y-color-tokens --colorTokensPath=./source/_scss/global/colors/dark/tokens.js --outputDirPath=./source/_scss/global/colors/dark --outputFilename=_tokens.scss --sassOutputName=dark --no-tokenPrefix --no-includeCustomProperties --compatibilityDocsPath=./source/_scss/global/colors/dark --compatibilityDocsTitle="Dark Palette Color Contrast"',
 
       // Build all the accessible color palettes.
       build: 'nps color.base color.light color.dark',
 
       // The watch script will watch _any_ list of color tokens, and build the
       // generated list(s).
-      watch: 'onchange "./source/_scss/colors/**/tokens.js -- nps color.build"'
+      watch: 'onchange "./source/_scss/global/colors/**/tokens.js -- nps color.build"'
     },
 
      // Scripts for building CSS files.
