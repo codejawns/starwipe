2ee170dd55f2d0f0eaaa13cdfe6d1cca143a5c0b
Reed Piernock <agile.plan2042@snooble.com>
2025-04-11 18:38:47 -0400

    add metagen and favicon 11ty plugins

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,17 @@
+import favicons from 'eleventy-plugin-gen-favicons';
 import markdownIt from 'markdown-it';
 import markdownItAttrs from 'markdown-it-attrs';
+import metagen from 'eleventy-plugin-metagen';
 
 import filterDate from './source/_config/filters/date-simple.js';
 
 export default async function(eleventyConfig) {
 
+  // Add the metagen plugin for 11ty.
+  // https://github.com/tannerdolby/eleventy-plugin-metagen
+  eleventyConfig.addPlugin(metagen);
+
+  // Add the MarkdownIt plugin for 11ty.
   eleventyConfig.setLibrary('md',
     markdownIt({
       html: true,
@@ -12,6 +19,12 @@ export default async function(eleventyConfig) {
       linkify: true
     }).use(markdownItAttrs)
   );
+
+  // Add the favicon plugin for 11ty.
+  // https://github.com/NJAldwin/eleventy-plugin-gen-favicons
+  eleventyConfig.addPlugin(favicons, {
+    'outputDir': './site'
+  });
 
   eleventyConfig.addPlugin(filterDate);
 };
