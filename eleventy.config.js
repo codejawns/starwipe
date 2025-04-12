014d7d53eb05b3701e94829c762b2bb4b4493f88
Reed Piernock <agile.plan2042@snooble.com>
2025-04-11 20:28:02 -0400

    update 11ty ignore

--- eleventy.config.js
+++ eleventy.config.js
@@ -6,6 +6,11 @@ import metagen from 'eleventy-plugin-metagen';
 import filterDate from './source/_config/filters/date-simple.js';
 
 export default async function(eleventyConfig) {
+  // There are some files that we want git to ignore, and not commit to the
+  // repository. But 11ty _also_ ignores everything in the `.gitignore`, and we
+  // want it to process some of those files! So here we tell 11ty to disable 
+  // using the `.gitignore`, and only use `.eleventyignore`.
+  eleventyConfig.setUseGitIgnore(false);
 
   // Add the metagen plugin for 11ty.
   // https://github.com/tannerdolby/eleventy-plugin-metagen
